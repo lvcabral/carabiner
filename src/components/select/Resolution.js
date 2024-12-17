242da47beee72d8a964412788e79c5188f255298
Marcelo Cabral <marcelo.cabral@paramount.com>
2024-12-16 20:19:10 -0700

    Persisted the Transparency filter setting

--- src/components/select/Resolution.js
+++ src/components/select/Resolution.js
@@ -2,13 +2,40 @@ import React from "react";
 import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
 
-function Resolution({ resolutions, value, onChange }) {
+function Resolution({ value, onChange }) {
+  const resolutionOptions = [
+    {
+      value: "480px|270px",
+      label: "480 x 270",
+    },
+    {
+      value: "640px|360px",
+      label: "640 x 360",
+    },
+    {
+      value: "800px|450px",
+      label: "800 x 450",
+    },
+    {
+      value: "1024px|576px",
+      label: "1024 x 576",
+    },
+    {
+      value: "1280px|720px",
+      label: "1280 x 720",
+    },
+    {
+      value: "1920px|1080px",
+      label: "1920 x 1080",
+    },
+  ];
+
   return (
     <Card.Text as="div">
       <Form.Group controlId="formVideoResolution">
         <Form.Label>Resolution</Form.Label>
         <Form.Control as="select" value={value} onChange={onChange}>
-          {resolutions.map((resolution) => (
+          {resolutionOptions.map((resolution) => (
             <option key={resolution.value} value={resolution.value}>
               {resolution.label}
             </option>
