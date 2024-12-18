54803fca15452a22a865b734471cf89412117709
Marcelo Cabral <marcelo.cabral@paramount.com>
2024-12-17 18:02:30 -0700

    Refactored ADB keys mapping

--- public/adb.js
+++ public/adb.js
@@ -6,7 +6,7 @@ let isADBConnected = false;
 
 function connectADB(deviceIp) {
     try {
-        exec(adbPath + " connect " + deviceIp, puts);
+        exec(`${adbPath} connect ${deviceIp}`, puts);
         isADBConnected = true;
         console.log("Connected to ADB in " + deviceIp);
     } catch (error) {
@@ -17,7 +17,7 @@ function connectADB(deviceIp) {
 
 function disconnectADB() {
     try {
-        exec(adbPath + " disconnect", puts);
+        exec(`${adbPath} disconnect`, puts);
         isADBConnected = false;
         console.log("Disconnected from ADB");
     } catch (error) {
@@ -28,31 +28,7 @@ function disconnectADB() {
 
 function sendADBKey(key) {
     if (isADBConnected && typeof key === "string") {
-        if (key === "up") {
-            exec(adbPath + " shell input keyevent 19", puts);
-        } else if (key === "down") {
-            exec(adbPath + " shell input keyevent 20", puts);
-        } else if (key === "left") {
-            exec(adbPath + " shell input keyevent 21", puts);
-        } else if (key === "right") {
-            exec(adbPath + " shell input keyevent 22", puts);
-        } else if (key === "select") {
-            exec(adbPath + " shell input keyevent 66", puts);
-        } else if (key === "back") {
-            exec(adbPath + " shell input keyevent 4", puts);
-        } else if (key === "home") {
-            exec(adbPath + " shell input keyevent 3", puts);
-        } else if (key === "info") {
-            exec(adbPath + " shell input keyevent 1", puts);
-        } else if (key === "rev") {
-            exec(adbPath + " shell input keyevent 89", puts);
-        } else if (key === "play") {
-            exec(adbPath + " shell input keyevent 85", puts);
-        } else if (key === "fwd") {
-            exec(adbPath + " shell input keyevent 90", puts);
-        } else if (key === "volumemute") {
-            exec(adbPath + " shell input keyevent 164", puts);
-        }
+        exec(`${adbPath} shell input keyevent ${key}`, puts);
         console.log(key + " pressed.");
     }
 }
