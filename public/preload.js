f0129b565a56705b187837a50c76a855e57771cd
Marcelo Lv Cabral <marcelo@lvcabral.com>
2024-12-17 22:28:31 -0700

    Added button to show the settings when hidden

--- public/preload.js
+++ public/preload.js
@@ -5,4 +5,5 @@ contextBridge.exposeInMainWorld("electronAPI", {
   send: (chan, message) => ipcRenderer.send(chan, message),
   invoke: (chan, message) => ipcRenderer.invoke(chan, message),
   onMessageReceived: (chan, callback) => ipcRenderer.on(chan, callback),
+  showSettings: () => ipcRenderer.send('show-settings'),
 });
